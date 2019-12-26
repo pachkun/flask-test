1dcb84804d7a1d10300516e87118dfe103b5d31f
pachkun <pachkunishka@gmail.com>
2019-12-26 12:17:27 +0300

    v0.4

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,15 @@
 __author__ = 'pachkun'
 
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from . import routes
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
--- config.py
+++ config.py
@@ -2,7 +2,12 @@
 __author__ = 'pachkun'
 
 import os
+from pathlib import Path
+
+basedir = Path().cwd()
 
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + str(basedir / 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
