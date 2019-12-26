c9464ca46e7b1ff3656f39e062f00923a378e124
pachkun <pachkunishka@gmail.com>
2019-12-26 15:10:32 +0300

    v0.5

--- app/models.py
+++ app/models.py
@@ -3,10 +3,13 @@ __author__ = 'pachkun'
 
 from datetime import datetime
 
-from . import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from . import db, login
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -15,6 +18,12 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}>'
+
+    def set_password(self, password: str):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password: str) -> bool:
+        return check_password_hash(self.password_hash, password)
 
 
 class Post(db.Model):
@@ -25,3 +34,8 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'<Post {self.body}>'
+
+
+@login.user_loader
+def load_user(id: str) -> User:
+    return User.query.get(int(id))
